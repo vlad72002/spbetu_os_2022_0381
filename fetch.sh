70f178003709887b039044782bfceaf47da8c9a1
Mikhail Efremov <jakutenshi@gmail.com>
2022-02-11 11:31:17 +0300

    Add all students to the table and scripts

--- fetch.sh
+++ fetch.sh
@@ -1,30 +1,26 @@
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
-#git fetch    #
+git fetch vlad72002       # Балакин       
+git fetch vbezuprechnaya  # Березовская   
+git fetch leoI0II         # Дзаппала      
+git fetch NikitaEfimov0   # Ефимов        
+git fetch Philipoff       # Захаров       
+git fetch BrodyWar        # Ибатов        
+git fetch IoninaKsu       # Ионина        
+git fetch KiriltsevDaniil # Кирильцев     
+git fetch Pavel7004       # Ковалёв       
+git fetch elithium4       # Короткина     
+git fetch Dmitr-Kotov     # Котов         
+git fetch Sedoy1          # Крашенинников 
+git fetch magnitovsergey  # Магнитов      
+git fetch V1xtor          # Михайлов      
+git fetch Pavloveuge      # Павлов        
+git fetch lexapech        # Печеркин      
+git fetch Timur0381       # Просекин      
+git fetch Lixonik         # Прохоров      
+git fetch RichSweetWafer  # Самойлов      
+git fetch SarychevaAA     # Сарычева      
+git fetch N3xTr1Me        # Соколов       
+git fetch Mordekay15      # Степанова     
+git fetch brokkko         # Странникова   
+git fetch techi9          # Хмарский      
+git fetch adoomaxas       # Шиняева       
+git fetch freezge         # Шыныбаев      
--- remotes.sh
+++ remotes.sh
@@ -1,30 +1,26 @@
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
-#git remote add                           https://github.com//spbetu_os_2022_0381.git            # 
+git remote add  vlad72002       https://github.com/vlad72002/spbetu_os_2022_0381.git       # Балакин       
+git remote add  vbezuprechnaya  https://github.com/vbezuprechnaya/spbetu_os_2022_0381.git  # Березовская   
+git remote add  leoI0II         https://github.com/leoI0II/spbetu_os_2022_0381.git         # Дзаппала      
+git remote add  NikitaEfimov0   https://github.com/NikitaEfimov0/spbetu_os_2022_0381.git   # Ефимов        
+git remote add  Philipoff       https://github.com/Philipoff/spbetu_os_2022_0381.git       # Захаров       
+git remote add  BrodyWar        https://github.com/BrodyWar/spbetu_os_2022_0381.git        # Ибатов        
+git remote add  IoninaKsu       https://github.com/IoninaKsu/spbetu_os_2022_0381.git       # Ионина        
+git remote add  KiriltsevDaniil https://github.com/KiriltsevDaniil/spbetu_os_2022_0381.git # Кирильцев     
+git remote add  Pavel7004       https://github.com/Pavel7004/spbetu_os_2022_0381.git       # Ковалёв       
+git remote add  elithium4       https://github.com/elithium4/spbetu_os_2022_0381.git       # Короткина     
+git remote add  Dmitr-Kotov     https://github.com/Dmitr-Kotov/spbetu_os_2022_0381.git     # Котов         
+git remote add  Sedoy1          https://github.com/Sedoy1/spbetu_os_2022_0381.git          # Крашенинников 
+git remote add  magnitovsergey  https://github.com/magnitovsergey/spbetu_os_2022_0381.git  # Магнитов      
+git remote add  V1xtor          https://github.com/V1xtor/spbetu_os_2022_0381.git          # Михайлов      
+git remote add  Pavloveuge      https://github.com/Pavloveuge/spbetu_os_2022_0381.git      # Павлов        
+git remote add  lexapech        https://github.com/lexapech/spbetu_os_2022_0381.git        # Печеркин      
+git remote add  Timur0381       https://github.com/Timur0381/spbetu_os_2022_0381.git       # Просекин      
+git remote add  Lixonik         https://github.com/Lixonik/spbetu_os_2022_0381.git         # Прохоров      
+git remote add  RichSweetWafer  https://github.com/RichSweetWafer/spbetu_os_2022_0381.git  # Самойлов      
+git remote add  SarychevaAA     https://github.com/SarychevaAA/spbetu_os_2022_0381.git     # Сарычева      
+git remote add  N3xTr1Me        https://github.com/N3xTr1Me/spbetu_os_2022_0381.git        # Соколов       
+git remote add  Mordekay15      https://github.com/Mordekay15/spbetu_os_2022_0381.git      # Степанова     
+git remote add  brokkko         https://github.com/brokkko/spbetu_os_2022_0381.git         # Странникова   
+git remote add  techi9          https://github.com/techi9/spbetu_os_2022_0381.git          # Хмарский      
+git remote add  adoomaxas       https://github.com/adoomaxas/spbetu_os_2022_0381.git       # Шиняева       
+git remote add  freezge         https://github.com/freezge/spbetu_os_2022_0381.git         # Шыныбаев      
